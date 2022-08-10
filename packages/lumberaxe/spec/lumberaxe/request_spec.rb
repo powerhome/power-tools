9ceca44220a0c1a264a07fac2549e48adee44953
Carlos Palhares <chjunior@gmail.com>
2022-08-10 12:09:52 -0300

    Add rubocop to CI

--- packages/lumberaxe/spec/lumberaxe/request_spec.rb
+++ packages/lumberaxe/spec/lumberaxe/request_spec.rb
@@ -4,6 +4,6 @@ require "rails_helper"
 
 RSpec.describe "Lumberaxe integration" do
   it "prints to stdout" do
-    expect { print('foo') }.to output(/foo/).to_stdout
+    expect { print("foo") }.to output(/foo/).to_stdout
   end
 end
