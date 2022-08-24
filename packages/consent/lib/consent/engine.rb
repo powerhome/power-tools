e92928f4f762a6229ada941d2341f7765fabc863
Carlos Palhares <chjunior@gmail.com>
2022-08-24 17:49:15 -0300

    Remove dead code

--- packages/consent/lib/consent/engine.rb
+++ packages/consent/lib/consent/engine.rb
@@ -25,13 +25,5 @@ module Consent
         include Consent::ModelAdditions
       end
     end
-
-    # initializer "consent.append_migrations" do |app|
-    #   unless app.root.to_s.match? root.to_s
-    #     config.paths["db/migrate"].expanded.each do |expanded_path|
-    #       app.config.paths["db/migrate"] << expanded_path
-    #     end
-    #   end
-    # end
   end
 end
