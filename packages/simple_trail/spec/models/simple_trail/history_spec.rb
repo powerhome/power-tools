87e09d832abefc70179b0454700fc17086f0c3a7
Gurban Haydarov <gurban@hey.com>
2023-03-17 14:30:38 -0400

    Fix a rubocop issue

--- packages/simple_trail/spec/models/simple_trail/history_spec.rb
+++ packages/simple_trail/spec/models/simple_trail/history_spec.rb
@@ -3,8 +3,8 @@
 require "rails_helper"
 
 RSpec.describe SimpleTrail::History, type: :model do
-  let!(:note_1) { SimpleTrail::History.create(source_id: 1, source_type: "Test", note: "Note 1") }
-  let!(:note_2) { SimpleTrail::History.create(source_id: 2, source_type: "Test", note: "Note 2") }
+  let!(:note_one) { SimpleTrail::History.create(source_id: 1, source_type: "Test", note: "Note 1") }
+  let!(:note_two) { SimpleTrail::History.create(source_id: 2, source_type: "Test", note: "Note 2") }
 
   it "serializes the source changes" do
     history = SimpleTrail::History.create(source_changes: { "id" => [nil, 10] })
@@ -16,18 +16,18 @@ RSpec.describe SimpleTrail::History, type: :model do
 
   describe ".in_natural_order" do
     it "returns history collection in natural order" do
-      expect(SimpleTrail::History.in_natural_order).to eq([note_1, note_2])
+      expect(SimpleTrail::History.in_natural_order).to eq([note_one, note_two])
     end
   end
 
   describe ".for_source" do
     it "returns history collection in natural order" do
       Test = Struct.new(:id)
-      expect(SimpleTrail::History.for_source(Test.new(1))).to eq([note_1])
+      expect(SimpleTrail::History.for_source(Test.new(1))).to eq([note_one])
     end
   end
 
   it "returns history collection in reverse chronological order" do
-    expect(SimpleTrail::History.all).to eq([note_2, note_1]) # applies default_scope
+    expect(SimpleTrail::History.all).to eq([note_two, note_one]) # applies default_scope
   end
 end
