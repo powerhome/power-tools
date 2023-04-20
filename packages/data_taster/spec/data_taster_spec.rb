cf3a33ff29d1a81260f856dce9801df5b369b0fd
Jill Klang <jill.klang@powerhrg.com>
2023-04-20 14:34:03 -0400

    This package is a placeholder for now

--- packages/data_taster/spec/data_taster_spec.rb
+++ packages/data_taster/spec/data_taster_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe DataTaster do
   it "has a version number" do
     expect(DataTaster::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
