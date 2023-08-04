43af5d3031c6410fb2bae7879fde0305ea8d867b
Carlos Palhares <chjunior@gmail.com>
2023-08-04 14:06:50 -0300

    Lower rails version for migration

--- packages/audiences/db/migrate/20230725194934_create_audiences_contexts.rb
+++ packages/audiences/db/migrate/20230725194934_create_audiences_contexts.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreateAudiencesContexts < ActiveRecord::Migration[7.0]
+class CreateAudiencesContexts < ActiveRecord::Migration[6.0]
   def change
     create_table :audiences_contexts do |t|
       t.references :owner, polymorphic: true, null: false
