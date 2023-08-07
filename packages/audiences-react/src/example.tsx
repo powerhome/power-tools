73d2a42b24e6b678a5bcb91d84b65f86266d1643
Carlos Palhares <chjunior@gmail.com>
2023-08-07 17:34:45 -0300

    Switch example to use scim proxy

--- packages/audiences-react/src/example.tsx
+++ packages/audiences-react/src/example.tsx
@@ -12,7 +12,7 @@ import { AudienceEditor } from "./AudienceEditor"
 const audienceKey =
   "BAh7CEkiCGdpZAY6BkVUSSIfZ2lkOi8vZHVtbXkvRXhhbXBsZU93bmVyLzEGOwBUSSIMcHVycG9zZQY7AFRJIg5hdWRpZW5jZXMGOwBUSSIPZXhwaXJlc19hdAY7AFRJIh0yMDIzLTA5LTAyVDE4OjE1OjQxLjQxNloGOwBU--7bcda79e962d221c4820cc4afebd194d288d7dc5"
 const audiencContextUri = `http://localhost:3000/audiences/${audienceKey}`
-const scimV2Uri = `http://localhost:3002/api/scim/v2`
+const scimV2Uri = `http://localhost:3000/audiences/scim/`
 
 const rootNode = document.getElementById("root")
 ReactDOM.render(
