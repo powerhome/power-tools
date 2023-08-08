5b44b75a871672d43895eac891591872da874065
Carlos Palhares <chjunior@gmail.com>
2023-08-08 14:00:34 -0300

    Fix rubocop violations

--- packages/audiences/spec/dummy/app/controllers/example_owners_controller.rb
+++ packages/audiences/spec/dummy/app/controllers/example_owners_controller.rb
@@ -1,37 +1,35 @@
 # frozen_string_literal: true
 
 class ExampleOwnersController < ApplicationController
-  before_action :set_example_owner, only: %i[ show edit update destroy ]
-  
+  before_action :set_example_owner, only: %i[show edit update destroy]
+
   # GET /example_owners
   def index
     @example_owners = ExampleOwner.all
   end
-  
+
   # GET /example_owners/1
-  def show
-  end
-  
+  def show; end
+
   # GET /example_owners/new
   def new
     @example_owner = ExampleOwner.new
   end
-  
+
   # GET /example_owners/1/edit
-  def edit
-  end
-  
+  def edit; end
+
   # POST /example_owners
   def create
     @example_owner = ExampleOwner.new(example_owner_params)
-    
+
     if @example_owner.save
       redirect_to @example_owner, notice: "Example owner was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   # PATCH/PUT /example_owners/1
   def update
     if @example_owner.update(example_owner_params)
@@ -40,19 +38,20 @@ class ExampleOwnersController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
   # DELETE /example_owners/1
   def destroy
     @example_owner.destroy
     redirect_to example_owners_url, notice: "Example owner was successfully destroyed.", status: :see_other
   end
-  
-  private
+
+private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_example_owner
     @example_owner = ExampleOwner.find(params[:id])
   end
-  
+
   # Only allow a list of trusted parameters through.
   def example_owner_params
     params.require(:example_owner).permit(:name)
