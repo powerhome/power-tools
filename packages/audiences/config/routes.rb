05a6f86de60e49a2b4eb0b0ab0d063cf82aaffbc
Carlos Palhares <chjunior@gmail.com>
2023-08-08 11:25:47 -0300

    Simpler audiences URL helpers

--- packages/audiences/config/routes.rb
+++ packages/audiences/config/routes.rb
@@ -2,5 +2,17 @@
 
 Audiences::Engine.routes.draw do
   get "/scim(/*scim_path)" => "scim_proxy#get", as: :scim_proxy
-  get "/:key" => "contexts#show", as: :context
+  get "/:key" => "contexts#show", as: :signed_context
+end
+
+Rails.application.routes.draw do
+  mount ::Audiences::Engine, at: "/audiences", as: :audiences
+  
+  direct :audience_context do |owner, options|
+    audiences.route_for(:signed_context, key: Audiences.sign(owner), **options)
+  end
+
+  direct :audience_scim_proxy do
+    audiences.route_for(:scim_proxy)
+  end
 end
--- packages/audiences/spec/dummy/config/routes.rb
+++ packages/audiences/spec/dummy/config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Audiences::Engine => "/audiences"
-
   resources :example_owners, only: %i[show]
 end
