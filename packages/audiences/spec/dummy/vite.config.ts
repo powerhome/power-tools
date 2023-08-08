b3d7fd31e9ea0ec3ed05368ae43e5a3baf404c0e
Carlos Palhares <chjunior@gmail.com>
2023-08-08 16:42:42 -0300

    Fix double rails with vite react plugin

--- packages/audiences/spec/dummy/vite.config.ts
+++ packages/audiences/spec/dummy/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import RubyPlugin from "vite-plugin-ruby";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [RubyPlugin()],
+  plugins: [RubyPlugin(), react()],
 });
