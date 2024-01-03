fcf3c9f226a9d67d673a590df92f61c65b650ab7
Jill Klang <jill.klang@powerhrg.com>
2024-01-03 11:52:38 -0500

    Initial setup for new gem

---
+++ packages/api_chai/spec/api_chai_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ApiChai do
+  it "has a version number" do
+    expect(ApiChai::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
