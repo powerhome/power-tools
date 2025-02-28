67544eb3af7c29e23848a34ec3f17abaf4f3bea1
Vinicius Dittgen <vinicius.dittgen@powerhrg.com>
2025-02-28 11:45:25 -0300

    Add factory register

--- packages/dw_connector/lib/dw_connector.rb
+++ packages/dw_connector/lib/dw_connector.rb
@@ -5,6 +5,9 @@ require_relative "dw_connector/data_warehouse_repository"
 require_relative "dw_connector/repository_factory"
 require_relative "dw_connector/adapters/trino_repository"
 
+# Register default adapters
+DWConnector::RepositoryFactory.register(:trino, DWConnector::Adapters::TrinoRepository)
+
 module DWConnector
   class Error < StandardError; end
 end
--- packages/dw_connector/lib/dw_connector/repository_factory.rb
+++ packages/dw_connector/lib/dw_connector/repository_factory.rb
@@ -3,6 +3,14 @@
 module DWConnector
   class RepositoryFactory
     class << self
+      def repositories
+        @repositories ||= {}
+      end
+
+      def register(type, repository_class)
+        repositories[type.to_sym] = repository_class
+      end
+
       def create(table_name:, type: :trino, conditions: nil, config: {})
         repository_class = repository_for(type)
         repository_class.new(table_name, conditions, config)
@@ -11,16 +19,11 @@ module DWConnector
     private
 
       def repository_for(type)
-        case type
-        when :trino
-          Adapters::TrinoRepository
-        else
-          raise ArgumentError, "Unsupported repository type: #{type}. Available types: #{available_types.join(', ')}"
-        end
-      end
-
-      def available_types
-        [:trino]
+        type = type.to_sym
+        repositories[type] || raise(
+          ArgumentError,
+          "Unsupported repository type: #{type}. Available types: #{repositories.keys.join(', ')}"
+        )
       end
     end
   end
