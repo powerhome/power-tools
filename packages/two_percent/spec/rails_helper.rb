78a016cedf15b12d7e967944cffa6f473687fb2a
Carlos Palhares <chjunior@gmail.com>
2025-03-24 20:46:10 +0000

    Add functional dummy app

--- packages/two_percent/spec/rails_helper.rb
+++ packages/two_percent/spec/rails_helper.rb
@@ -2,8 +2,8 @@
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
+ENV["RAILS_ENV"] = "test"
+require_relative "dummy/config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
