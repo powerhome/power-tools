47edb86c6d75ed62d3433ce6e6bf5d3c71d0d8fe
Carlos Palhares <chjunior@gmail.com>
2025-03-25 19:13:13 +0000

    Require engine from dummy app

--- packages/two_percent/spec/dummy/config/application.rb
+++ packages/two_percent/spec/dummy/config/application.rb
@@ -4,6 +4,7 @@ require_relative "boot"
 require "logger"
 
 require "action_controller/railtie"
+require "two_percent/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
