744fc6d1edea9dda8e0fe2faf2ef17fdad2695b3
Patrick McClernan <patrick.mcclernan@powerhrg.com>
2025-09-09 12:56:59 -0400

    Format a bit

--- packages/data_taster/lib/data_taster/critic.rb
+++ packages/data_taster/lib/data_taster/critic.rb
@@ -4,37 +4,85 @@ module DataTaster
   # Tracks the time, row count, and total size by table
   class Critic
     def initialize
-      @reviews = {}
+      @reviews = []
     end
 
     def track_dump(&block)
       bm = Benchmark.measure(&block)
 
-      log_info("Dump completed in #{bm.real} seconds")
+      log_info("Dump completed in #{bm.real.round(4)} seconds")
 
-      @reviews.each do |table_name, review|
-        log_info("Table #{table_name} completed in #{review[:time]} seconds")
-      end
-
-      report_slowest_table
+      report_exceptional_tables
     end
 
     def track_table(table_name, &block)
       bm = Benchmark.measure(&block)
 
-      @reviews[table_name] = {
-        time: bm.real,
+      review = {
+        table_name: table_name,
+        time: bm.real.round(4),
+        rows:  DataTaster.safe_execute("SELECT COUNT(*) FROM #{table_name}").first["COUNT(*)"],
+        size: DataTaster.safe_execute(table_size_sql(table_name)).first["size_mb"],
       }
+
+      @reviews << review
+
+      log_horizontal_rule
+      publish(review)
+      log_horizontal_rule
     end
 
   private
 
-    def report_slowest_table
+    def table_size_sql(table_name)
+      <<-SQL.squish
+        SELECT ROUND(((DATA_LENGTH + INDEX_LENGTH) / 1024 / 1024), 2) as size_mb
+        FROM information_schema.tables WHERE table_name = '#{table_name}'
+      SQL
+    end
+
+    def publish(review)
+      size = review[:size]
+      rows = review[:rows]
+      duration = review[:time]
+
+      log_info("Table #{review[:table_name]} completed in #{duration} seconds," \
+               " dumped #{rows} #{"row".pluralize(rows)} and #{size} MB of data")
+    end
+
+    def log_horizontal_rule
+      log_info("--------------------------------")
+    end
+
+    def report_exceptional_tables
+      log_horizontal_rule
+      report_slowest_tables
+      report_largest_tables_by_size
+      report_largest_tables_by_rows
+    end
+
+    def report_slowest_tables
       log_info("Slowest tables:")
 
-      @reviews.sort_by { |_, review| review[:time] }.last(5).each do |table_name, review|
-        log_info("Table #{table_name} completed in #{review[:time]} seconds")
-      end
+      log_horizontal_rule
+      @reviews.sort_by { |review| review[:time] }.reverse.last(5).each { |review| publish(review) }
+      log_horizontal_rule
+    end
+
+    def report_largest_tables_by_size
+      log_info("Largest tables by size:")
+
+      log_horizontal_rule
+      @reviews.sort_by { |review| review[:size] }.reverse.last(5).each { |review| publish(review) }
+      log_horizontal_rule
+    end
+
+    def report_largest_tables_by_rows
+      log_info("Largest tables by rows:")
+
+      log_horizontal_rule
+      @reviews.sort_by { |review| review[:rows] }.reverse.last(5).each { |review| publish(review) }
+      log_horizontal_rule
     end
 
     def log_info(message)
