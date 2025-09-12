61b144c8c14106a6686d4eea9ad6a561a9cf5194
Patrick McClernan <patrick.mcclernan@powerhrg.com>
2025-09-12 10:53:47 -0400

    Attempt to better encapsulate db setup

--- packages/data_taster/spec/support/database_helper.rb
+++ packages/data_taster/spec/support/database_helper.rb
@@ -1,49 +1,56 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/module/delegation"
+
 # Helper methods to get database configurations, accounting for changes between rails versions
 module DatabaseHelper
-  def source_db_config
-    { key: "primary", name: "test_source" }
-  end
+  class DBConfig
+    VALUES = {
+      source: { key: "primary", name: "test_source" },
+      dump: { key: "test_dump", name: "test_dump" },
+    }.freeze
 
-  def dump_db_config
-    { key: "test_dump", name: "test_dump" }
-  end
+    def dump_db_name
+      VALUES[:dump][:name]
+    end
 
-  def dump_db_name
-    dump_db_config[:name]
-  end
+    def dump_db_client
+      @dump_db_client ||= Mysql2::Client.new(test_dump_database_config)
+    end
 
-  def dump_db_config_key
-    dump_db_config[:key]
-  end
+    def test_dump_database_config
+      ActiveRecord::Base.configurations.configs_for(env_name: Rails.env,
+                                                    name: VALUES[:dump][:key]).configuration_hash
+    rescue ArgumentError
+      ActiveRecord::Base.configurations.configs_for(env_name: Rails.env,
+                                                    spec_name: VALUES[:dump][:key]).config
+    end
 
-  def dump_db_client
-    @dump_db_client ||= Mysql2::Client.new(test_dump_database_config)
-  end
+    def source_db_name
+      VALUES[:source][:name]
+    end
 
-  def source_db_name
-    source_db_config[:name]
-  end
+    def source_db_client
+      @source_db_client ||= Mysql2::Client.new(test_source_database_config)
+    end
 
-  def source_db_config_key
-    source_db_config[:key]
+    def test_source_database_config
+      ActiveRecord::Base.configurations.configs_for(env_name: Rails.env,
+                                                    name: VALUES[:source][:key]).configuration_hash
+    rescue ArgumentError
+      ActiveRecord::Base.configurations.configs_for(env_name: Rails.env,
+                                                    spec_name: VALUES[:source][:key]).config
+    end
   end
+  private_constant :DBConfig
 
-  def source_db_client
-    @source_db_client ||= Mysql2::Client.new(test_source_database_config)
-  end
+  # Methods available for use during specs
+  delegate :dump_db_name, :dump_db_client, :source_db_name, :source_db_client, to: :db_config
 
-  def test_source_database_config
-    ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, name: source_db_config_key).configuration_hash
-  rescue ArgumentError
-    ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, spec_name: source_db_config_key).config
-  end
+private
 
-  def test_dump_database_config
-    ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, name: dump_db_config_key).configuration_hash
-  rescue ArgumentError
-    ActiveRecord::Base.configurations.configs_for(env_name: Rails.env, spec_name: dump_db_config_key).config
+  def db_config
+    @db_config ||= DBConfig.new
   end
 end
 
